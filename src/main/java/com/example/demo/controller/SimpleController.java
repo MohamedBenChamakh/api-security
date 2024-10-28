009d118451561e03e7a51ac5bfabfb2fde196bc4
Mohamed Ben Chamakh <mohamed.ben.chamakh98@gmail.com>
2024-10-28 11:16:10 +0100

    manage SecurityFilterChain

--- src/main/java/com/example/demo/controller/SimpleController.java
+++ src/main/java/com/example/demo/controller/SimpleController.java
@@ -2,14 +2,22 @@ package com.example.demo.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api")
 @RestController
 public class SimpleController {
 
 
-    @GetMapping("/api/hello")
+    @GetMapping("/hello")
     public ResponseEntity<String> sayHelloWorld(){
         return new ResponseEntity<>("HelloWorld",HttpStatus.OK);
+    }
+
+
+    @GetMapping("/goodbye")
+    public ResponseEntity<String> sayGoodBye(){
+        return new ResponseEntity<>("GoodBye",HttpStatus.OK);
     }
 }
